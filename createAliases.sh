ffb647245d09f0d7585880a30f5bcd4c5a44a6e9
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-05-16 23:32:18 -0500

    Updating ignore files

---
+++ createAliases.sh
@@ -0,0 +1,12 @@
+sudo ln -s ~/projects/dotfiles/.ctags ~/.ctags
+sudo ln -s ~/projects/dotfiles/.git ~/.git
+sudo ln -s ~/projects/dotfiles/.gitconfig ~/.gitconfig
+sudo ln -s ~/projects/dotfiles/.gitignore_global ~/.gitignore_global
+sudo ln -s ~/projects/dotfiles/.gvimrc ~/.gvimrc
+sudo ln -s ~/projects/dotfiles/.hgignore ~/.hgignore
+sudo ln -s ~/projects/dotfiles/.hgignore_global ~/.hgignore_global
+sudo ln -s ~/projects/dotfiles/.hgrc ~/.hgrc
+sudo ln -s ~/projects/dotfiles/.ted_grails_complete ~/.ted_grails_complete
+sudo ln -s ~/projects/dotfiles/.vim ~/.vim
+sudo ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
+sudo ln -s ~/projects/dotfiles/.zshrc ~/.zshrc
