9c4a692a01a8c6d59db7edb4e03f190aa6d217d6
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-07-12 16:26:22 -0500

    Various updates

---
+++ dependencies/updateDependencies.sh
@@ -0,0 +1,6 @@
+cd ~/projects/dotfiles/dependencies/hg-prompt
+hg fetch
+cd ~/projects/dotfiles/dependencies/zsh-history-substring-search
+git pull
+cd ~/projects/dotfiles/dependencies/zsh-syntax-highlighting
+git pull
