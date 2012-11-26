7632f6f347c665143e7ebfe1e50e143250690d0d
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2012-11-26 16:43:46 -0600

    tweaks to .slate

--- createAliases.sh
+++ createAliases.sh
@@ -30,10 +30,10 @@ rm -Rf ~/.oh-my-zsh/themes/shaun.zsh-theme
 sudo ln -s ~/projects/dotfiles/.oh-my-zsh/themes/shaun.zsh-theme ~/.oh-my-zsh/themes/shaun.zsh-theme
 rm /Users/sjurgemeyer/Library/Application\ Support/KeyRemap4MacBook/private.xml
 sudo ln -s ~/projects/dotfiles/dependencies/private.xml ~/Library/Application\ Support/KeyRemap4MacBook/private.xml
-rm -Rf /usr/local/bin/grails
-sudo ln -s ~/projects/dotfiles/dependencies/grails /usr/local/bin/grails
-rm -Rf /usr/local/bin/grails-debug
-sudo ln -s ~/projects/dotfiles/dependencies/grails-debug /usr/local/bin/grails-debug
+rm -Rf /usr/local/bin/grails-dynamic
+sudo ln -s ~/projects/dotfiles/dependencies/grails /usr/local/bin/grails-dynamic
+rm -Rf /usr/local/bin/grails-debug-dynamic
+sudo ln -s ~/projects/dotfiles/dependencies/grails-debug /usr/local/bin/grails-debug-dynamic
 rm -Rf /usr/local/bin/grails-compile-file
 sudo ln -s ~/projects/dotfiles/dependencies/grails-compile-file /usr/local/bin/grails-compile-file
 rm -Rf /usr/local/bin/grails-version
