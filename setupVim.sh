2eb78ead93f32777ab348ec342e6fa8ddd472534
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2015-08-18 21:53:50 -0500

    initing submodules in setupVim

--- setupVim.sh
+++ setupVim.sh
@@ -2,6 +2,9 @@
 mkdir ~/projects
 cd ~/projects
 git clone https://github.com/sjurgemeyer/dotfiles
+cd dotfiles
+git submodule update --init
+cd ..
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 ~/projects/dotfiles/createVimAliases.sh
 vi +BundleInstall +qall
