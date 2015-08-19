d8cad2d1934bef1eb46cef9b3139daba7e0dec15
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2015-08-18 21:56:40 -0500

    trying to get vundle to work in setupVim

--- setupVim.sh
+++ setupVim.sh
@@ -5,7 +5,8 @@ git clone https://github.com/sjurgemeyer/dotfiles
 cd dotfiles
 git submodule update --init
 cd ..
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+mkdir ~/projects/dotfiles/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/projects/dotfiles/.vim/bundle/vundle
 ~/projects/dotfiles/createVimAliases.sh
 vi +BundleInstall +qall
 
