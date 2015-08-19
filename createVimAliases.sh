dcedc150e2fa5bc32735b804a20fdd4659135f98
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2015-08-18 21:43:19 -0500

    Removing sudos from vim aliases executable

--- createVimAliases.sh
+++ createVimAliases.sh
@@ -1,10 +1,10 @@
 rm -Rf ~/.gvimrc
-sudo ln -s ~/projects/dotfiles/.gvimrc ~/.gvimrc
+ln -s ~/projects/dotfiles/.gvimrc ~/.gvimrc
 rm -Rf ~/.vim
-sudo ln -s ~/projects/dotfiles/.vim ~/.vim
+ln -s ~/projects/dotfiles/.vim ~/.vim
 rm -Rf ~/.vimrc
-sudo ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
+ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
 rm -Rf ~/.ctags
-sudo ln -s ~/projects/dotfiles/.ctags ~/.ctags
+ln -s ~/projects/dotfiles/.ctags ~/.ctags
 rm -Rf ~/.vim/colors/ororo.vim
-sudo ln -s ~/projects/dotfiles/dependencies/vim-ororo/ororo.vim ~/.vim/colors/ororo.vim 
+ln -s ~/projects/dotfiles/dependencies/vim-ororo/ororo.vim ~/.vim/colors/ororo.vim 
