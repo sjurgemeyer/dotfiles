16db7e83ca8590504a60c9cfc287781f1fae918e
Shaun Jurgemeyer <sjurgemeyer@gmail.com>
2015-08-18 21:41:30 -0500

    Allowing download of vim files

--- createAliases.sh
+++ createAliases.sh
@@ -42,16 +42,7 @@ rm -Rf ~/.topydo
 sudo ln -s ~/projects/dotfiles/.topydo ~/.topydo
 
 #Vim
-rm -Rf ~/.gvimrc
-sudo ln -s ~/projects/dotfiles/.gvimrc ~/.gvimrc
-rm -Rf ~/.vim
-sudo ln -s ~/projects/dotfiles/.vim ~/.vim
-rm -Rf ~/.vimrc
-sudo ln -s ~/projects/dotfiles/.vimrc ~/.vimrc
-rm -Rf ~/.ctags
-sudo ln -s ~/projects/dotfiles/.ctags ~/.ctags
-rm -Rf ~/.vim/colors/ororo.vim
-sudo ln -s ~/projects/dotfiles/dependencies/vim-ororo/ororo.vim ~/.vim/colors/ororo.vim 
+~/projects/dotfiles/createVimAliases.sh
 
 # Used for syntastic
 sudo rm -Rf /usr/local/bin/grails-compile-file
---
+++ setupVim.sh
@@ -0,0 +1,8 @@
+#bash -c "$(curl -fsSkL raw.github.com/sjurgemeyer/dotfiles/master/setupVim.sh)"
+mkdir ~/projects
+cd ~/projects
+git clone https://github.com/sjurgemeyer/dotfiles
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+~/projects/dotfiles/createVimAliases.sh
+vi +BundleInstall +qall
+
